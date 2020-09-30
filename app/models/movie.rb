27027c6997c0563482d7187a98d9d57e5a9667e9
Flavio Trindade <fhtd04@gmail.com>
2020-09-30 19:10:42 -0300

    search implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+
+  include PgSearch::Model
+  pg_search_scope :search_by_titulo_no_brasil,
+  against: [ :titulo_no_brasil ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
