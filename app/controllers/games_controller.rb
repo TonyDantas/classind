586c984f3e729610e453ac9a2ca2aa988ca439a8
Flavio Trindade <fhtd04@gmail.com>
2020-10-01 14:32:21 -0300

    omdb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    if params[:query] && Game.search_by_titulo_no_brasil(params[:query]).present?
+      @games = Game.search_by_titulo_no_brasil(params[:query])
+    else
+      @games = Game.all
+    end
   end
 
   def show
