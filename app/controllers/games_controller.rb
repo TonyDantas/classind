0974e22c833d2497149bb05bbf053254dcac94cd
Eduardo Dantas <eduardopimenteldantas@hotmail.com>
2020-10-02 03:32:38 -0300

    controllers e home

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,13 @@ class GamesController < ApplicationController
   def index
     if params[:query] && Game.search_by_titulo_no_brasil(params[:query]).present?
       @games = Game.search_by_titulo_no_brasil(params[:query])
+    elsif session[:params] && Game.search_by_titulo_no_brasil(session[:params]).present?
+      @games = Game.search_by_titulo_no_brasil(session[:params])
+      session[:params] = false
     else
       @games = Game.all
+      flash[:notice] = "Título não localizado"
+      redirect_to root_path
     end
   end
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,18 @@
 class MoviesController < ApplicationController
 
   def index
-    if params[:query] && Movie.search_by_titulo_no_brasil(params[:query]).present?
-      @movies = Movie.search_by_titulo_no_brasil(params[:query])
-    elsif params[:query] && Game.search_by_titulo_no_brasil(params[:query]).present?
-      redirect_to games_path
-    else
-      @movies = Movie.all
+    case params[:commit] 
+    when "Audiovisual", "Buscar"
+      if params[:query] && Movie.search_by_titulo_no_brasil(params[:query]).present?
+        @movies = Movie.search_by_titulo_no_brasil(params[:query])
+      else
+        @movies = Movie.all
+        flash[:notice] = "Título não localizado"
+        redirect_to root_path
+      end
+    when "Jogos"
+      session[:params] = params[:query]
+      redirect_to games_path  
     end
   end
 
@@ -15,3 +21,18 @@ class MoviesController < ApplicationController
   end
 
 end
+
+
+
+# def index
+#   if params[:query] && Movie.search_by_titulo_no_brasil(params[:query]).present?
+#     @movies = Movie.search_by_titulo_no_brasil(params[:query])
+#   elsif params[:query] && Game.search_by_titulo_no_brasil(params[:query]).present?
+#     session[:params] = params[:query]
+#     redirect_to games_path
+#   else
+#     @movies = Movie.all
+#     flash[:notice] = "Título não localizado"
+#     redirect_to root_path
+#   end
+# end
