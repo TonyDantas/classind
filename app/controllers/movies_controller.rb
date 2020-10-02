5c39189b06506f08c1bb156902bc1ad88b7cc696
Antonio Dantas <antoniocrdantas@gmail.com>
2020-10-02 16:15:08 -0300

    Show views updated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,18 @@
 class MoviesController < ApplicationController
 
   def index
-    case params[:commit] 
+    case params[:commit]
     when "Audiovisual", "Buscar"
       if params[:query] && Movie.search_by_titulo_no_brasil(params[:query]).present?
         @movies = Movie.search_by_titulo_no_brasil(params[:query])
       else
-        @movies = Movie.all
         flash[:notice] = "Título não localizado"
-        redirect_to root_path
+        @movies = []
+        # redirect_to root_path
       end
     when "Jogos"
       session[:params] = params[:query]
-      redirect_to games_path  
+      redirect_to games_path
     end
   end
 
